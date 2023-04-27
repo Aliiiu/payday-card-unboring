a0d0302da86abf0f96bc868cfd8e7b3cd4520d94
goodness <goodness@revivar.app>
2023-04-27 12:06:12 +0100

    mobile: Card Featues section && Warp

--- app/pages/Home/index.js
+++ app/pages/Home/index.js
@@ -1,4 +1,5 @@
-import LongPage from "classes/LongPage";
+import LongPage from "classes/LongPage"
+import gsap from "gsap"
 
 export default class Home extends LongPage {
   constructor() {
@@ -6,15 +7,24 @@ export default class Home extends LongPage {
       element: ".home",
       id: "home",
       elements: {},
-    });
+    })
   }
 
   /** Life Cycle */
   create() {
-    super.create();
-    this.reCalculate({ scroll: {} });
+    super.create()
+    this.reCalculate({ scroll: {} })
+    gsap.to("feTurbulence", {
+      duration: 3,
+      repeat: 40,
+      yoyo: true,
+      ease: "none",
+      attr: {
+        baseFrequency: 0.005,
+      },
+    })
   }
   reCalculate() {
-    super.reCalculate({ scroll: {} });
+    super.reCalculate({ scroll: {} })
   }
 }
